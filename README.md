9c8875137702f302ad17e3c67b515c62b3dbb847
hellosurya1010 <ayrus4444@gmail.com>
2024-03-11 13:00:42 +0530

    Feat: creating the TOC extension.

---
+++ README.md
@@ -0,0 +1,9 @@
+# EpicTableOfContents
+
+A fresh Tiptap extension.
+
+## Installation
+
+```bash
+npm install epic-table-of-contents
+```
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Extension } from '@tiptap/core'
+
+const EpicTableOfContent = Extension.create({
+  name: 'EpicTableOfContent',
+})
+
+export { EpicTableOfContent }
+
+export default EpicTableOfContent
